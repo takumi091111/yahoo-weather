fcb9e25f0188b0eab5fd87a0de39c27d82bc89e9
Asamac <takumi091111@gmail.com>
2018-10-12 00:18:19 +0900

    リファクタリング

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 ## Overview
 Yahoo!天気のデータを取得するライブラリ
-## Using
-Weather.new("都道府県名","今日/明日/明後日")
-```
-require "./weather.rb"
 
-hoge = Weather.new("大阪","明後日")
+## Using
+Weather.new('都道府県名','今日/明日/明後日')
 ```
+require './weather.rb'
+
+hoge = Weather.new('大阪','明後日')
+```
+
 ## 予報を取得
 ```
-hoge.telop => "雨後曇"
+hoge.telop => '雨後曇'
 ```
+
 ## 最高気温を取得
 ```
-hoge.maxtemp => "20℃"
+hoge.maxtemp => 20
 ```
+
 ## 最低気温を取得
 ```
-hoge.mintemp => "15℃"
+hoge.mintemp => 15
 ```
--- weather.rb
+++ weather.rb
@@ -1,43 +1,55 @@
-require "date"
-require "rss"
+require 'date'
+require 'rss'
+require 'json'
 
 class Weather
+    BASE_URL = 'https://rss-weather.yahoo.co.jp/rss/days'.freeze
+    DATE_TABLE = {
+      :今日 => Date.today,
+      :明日 => Date.today + 1,
+      :明後日 => Date.today + 2
+    }.freeze
+
     def initialize(location_str, date_str)
-        url = "https://rss-weather.yahoo.co.jp/rss/days/"
-        date = DateTime.now
-        # 日付を設定
-        case date_str
-        when /今日/ then date += 0
-        when /明日/ then date += 1
-        when /明後日/ then date += 2
-        end
-        # リストファイルの位置を調べる
-        if FileTest.exists?("list.txt")
-            list = "list.txt"
-        else
-            list = "yahoo-weather/list.txt"
-        end
-        # 場所を元にRSSのリンクを取得
-        IO.foreach(list, encoding: "utf-8") do |line|
-            if line =~ /#{location_str}:(.*)$/
-                url = "#{url}#{Regexp.last_match(1)}.xml"
-            end
-        end
-        rss = RSS::Parser.parse(url)
-        rss.items.each do |item|
-            # 条件を満たさなければ、ループ脱出
-            next unless item.title =~ /#{date.day}日/
-            next unless md = item.title.match(/【.*】\s(.*)\s-\s(.*)\/(.*)\s-\s.*/)
-            @telop = Regexp.last_match(1)
-            @maxtemp = Regexp.last_match(2)
-            @mintemp = Regexp.last_match(3)
-        end
+      # 都道府県と県庁所在地IDの対応表を読み込む
+      location_list = get_location_list
+      # データ取得
+      location_id = location_list[location_str.to_sym]
+      date = DATE_TABLE[date_str.to_sym]
+      data = get_data(location_id, date)
+      @telop = data[:telop]
+      @maxtemp = data[:maxtemp].to_i
+      @mintemp = data[:mintemp].to_i
     end
 
-    # def hoge {return hoge}と同じ意味
+    public
+
     attr_reader :telop, :maxtemp, :mintemp
 
     def rain?
-        @telop =~ /雨/ ? true : false
+      @telop =~ /雨/ ? true : false
+    end
+
+    private
+
+    def get_location_list
+      # 都道府県と県庁所在地IDの対応表を読み込んで返す
+      json = File.read('location.json')
+      location_list = JSON.parse(json, {:symbolize_names => true})
+      return location_list.inject(&:merge)
+    end
+
+    def get_data(location_id, date)
+      # 指定したIDのデータを取得
+      rss = RSS::Parser.parse("#{BASE_URL}/#{location_id}.xml")
+      # 指定した日付でフィルタリング
+      data = rss.items.find{|item| item.title =~ /#{date.day}日/}
+      # 予報, 最高温度, 最低温度をハッシュで返す
+      data.title.match(/【.*】\s(.*)\s-\s(.*)℃\/(.*)℃\s-\s.*/)
+      return {
+        :telop => Regexp.last_match(1),
+        :maxtemp => Regexp.last_match(2),
+        :mintemp => Regexp.last_match(3)
+      }
     end
 end
